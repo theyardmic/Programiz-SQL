250d85b86f8215975bb95a535f31bd8fab8b2433
Strucker.eth <aberamacarleton@gmail.com>
2022-11-16 23:58:26 +0300

    SELECT-DISTINCT

--- SELECT-AND-OR-NOT.sql
+++ SELECT-AND-OR-NOT.sql
@@ -1,4 +1,7 @@
-SELECT model, YEAR, price, transmission FROM MercWorld WHERE price > 45000 AND transmission = "Automatic";
-
 -- SELECT-AND-OR-NOT.sql
-SELECT model, YEAR, price, transmission,fuelType FROM MercWorld WHERE (price > 45000 AND transmission = "Automatic") AND fuelType = "Petrol" OR "Hybrid";
+SELECT DISTINCT price, YEAR, model, transmission,fuelType 
+FROM MercWorld 
+WHERE (price > 45000 AND transmission = "Automatic") 
+AND fuelType = "Petrol" OR "Diesel";
+
+
