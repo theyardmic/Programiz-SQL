308307f9175f7b891f5014f0e42cf91f183773b2
Strucker.eth <aberamacarleton@gmail.com>
2022-11-16 23:44:46 +0300

    SELECT -AND-OR-NOT

--- SELECT-AND-OR-NOT.sql
+++ SELECT-AND-OR-NOT.sql
@@ -1,1 +1,4 @@
 SELECT model, YEAR, price, transmission FROM MercWorld WHERE price > 45000 AND transmission = "Automatic";
+
+-- SELECT-AND-OR-NOT.sql
+SELECT model, YEAR, price, transmission,fuelType FROM MercWorld WHERE (price > 45000 AND transmission = "Automatic") AND fuelType = "Petrol" OR "Hybrid";
