898c19feae44f9ba70c262acea42ebe4938c9fbb
Strucker.eth <aberamacarleton@gmail.com>
2022-11-23 15:18:11 +0300

    Altering columns

---
+++ Datasets/Alter-Table.sql
@@ -0,0 +1,21 @@
+Alter Table Cars
+DROP COLUMN curb_wgt;
+
+Alter Table Cars
+DROP COLUMN width;
+
+Alter Table Cars
+DROP COLUMN length;
+
+Alter Table Cars
+DROP COLUMN Insales;
+
+Alter Table Cars
+DROP COLUMN partition;
+
+Alter Table Cars
+DROP COLUMN wheelbase;
+
+Alter Table Cars
+DROP COLUMN height;
+
