e9c9f94ec09c087e17b2983711a746464027693f
Strucker.eth <aberamacarleton@gmail.com>
2022-11-23 15:28:44 +0300

    Restore Backed Up DBs

--- Datasets/Backup-Database.sql
+++ Datasets/Backup-Database.sql
@@ -3,3 +3,6 @@ Backup Database Auto-Stats to Disk = 'C:\Backup\Backup-Database.bak';
 
 --Back up new changes
 Backup Database Auto-Stats to Disk = 'C:\Backup\Backup-Database.bak' with DIFFERENTIAL;
+
+--Restore backed up a backed database
+Restore Database Auto-Stats from Disk = 'C:\Backup\Backup-Database.bak' with REPLACE;
