c18d9cc48033ad864088b01e304ad0fb56f1a1bf
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 02:48:59 -0800

    Update Check.sql
    Creating a custom check constraint

--- Check.sql
+++ Check.sql
@@ -3,4 +3,11 @@ CREATE TABLE Highest_EV_Sales(
   Sales BIGINT CHECK(Sales > 1000000)
   );
   
+ 
+  --alternatively
+  CREATE TABLE Best_EV_Sales(
+  Model_Id int REFERENCES EVs(Serial_No) ,
+  Sales BIGINT,
+  CONSTRAINT High_sales CHECK(Sales > 1000000)
+  );
   
