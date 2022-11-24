180380fc24cf26294a5879cf01107b0421fc9885
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 01:17:14 -0800

    Update LEFT JOIN.sql
    LEFT JOIN Concept Explanation

--- LEFT JOIN.sql
+++ LEFT JOIN.sql
@@ -1,3 +1,5 @@
+--The left Join combines matching values plus remaining rows from the first table in the query
+
 SELECT C.customer_id AS Client, C.first_name AS Name, O.amount AS Order_Amount
 FROM Customers AS C
 LEFT JOIN Orders AS O
