2595b1d4fc3f307764eef48b914bcbf8cf80878e
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 02:36:37 -0800

    Update Foreign Key.sql
    Using Alter to add or drop foreign Keys

--- Foreign Key.sql
+++ Foreign Key.sql
@@ -2,3 +2,7 @@ CREATE TABLE EV_Sales(
   Model_Id int REFERENCES EVs(Serial_No),
   Sales BIGINT
   );
+  
+--You can also add or drop foreign keys using ALTER TABLE
+    ALTER TABLE EV_Sales
+    ADD FOREIGN KEY (Model_Id) REFERENCES EVs(Serial_No);
