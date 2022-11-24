458ef1d6ef667aa7ee5605cfe33f4876558379a3
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 01:32:28 -0800

    Update FULL OUTER JOIN.sql
    Concept explanation

--- FULL OUTER JOIN.sql
+++ FULL OUTER JOIN.sql
@@ -1,3 +1,6 @@
+/*Combines matching values based on the PK like INNER JOIN but also includes remaining values 
+from the first (Left) and second(Right) table creating a "FULL OUTER JOIN" Effect */
+
 SELECT C.customer_id AS Client, C.first_name AS Name, O.amount AS Order_Amount
 FROM Customers AS C
 FULL JOIN Orders AS O
