3b02a25c6bddca07d83956d84a842f628c4cb7ae
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 01:25:28 -0800

    Update RIGHT JOIN.sql
    Concept Explanation

--- RIGHT JOIN.sql
+++ RIGHT JOIN.sql
@@ -1,3 +1,4 @@
+-- Combines matching values based on the PK like INNER JOIN but also includes remaining values from the second(Right) table
 SELECT C.customer_id AS Client, C.first_name AS Name, O.amount AS Order_Amount
 FROM Customers AS C
 RIGHT JOIN Orders AS O
