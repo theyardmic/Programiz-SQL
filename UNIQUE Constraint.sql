0f7d41ed4445f553c99da8e87e170b2a6e922221
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 02:02:57 -0800

    Update UNIQUE Constraint.sql
    Concept explanation + Unique Indexes

--- UNIQUE Constraint.sql
+++ UNIQUE Constraint.sql
@@ -1,5 +1,12 @@
+--Ensures there are no duplicate values in the column
 CREATE TABLE Themes (
 	Episode int(50) PRIMARY KEY NOT NULL UNIQUE,
   	Season int(50),
   	Track varchar(50)
 );
+
+
+--It can also be applied to indexes
+
+CREATE UNIQUE INDEX Episode_no
+ON Themes(Episode);
