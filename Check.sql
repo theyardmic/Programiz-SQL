18995edb326863f07a37d04813438023ee256b23
Lennox Matwere <93291951+strucker-eth@users.noreply.github.com>
2022-11-24 02:52:27 -0800

    Update Check.sql
    ALTER, DROP Operations with CHECK

--- Check.sql
+++ Check.sql
@@ -11,3 +11,22 @@ CREATE TABLE Highest_EV_Sales(
   CONSTRAINT High_sales CHECK(Sales > 1000000)
   );
   
+  
+  --alternatively
+  ALTER TABLE Best_Ev_Sales
+  ADD CONSTRAINT High_sales CHECK(Sales > 1000000)
+  
+  --OR
+  
+  ALTER TABLE Best_Ev_Sales
+  ADD CHECK(Sales > 1000000)
+
+
+--to drop the constraints
+ALTER TABLE Best_Ev_Sales
+DROP CONSTRAINT High_sales CHECK(Sales > 1000000)
+
+--or
+
+ALTER TABLE Best_Ev_Sales
+DROP  CHECK(Sales > 1000000)
