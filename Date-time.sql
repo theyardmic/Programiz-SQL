cb7b662c280e5d8650c15b63b5b5d81b751645cc
Strucker.eth <aberamacarleton@gmail.com>
2022-11-25 13:14:43 +0300

    Date-time datatype inSQL

---
+++ Date-time.sql
@@ -0,0 +1,22 @@
+--DATE/TIME Columns in mySQL Server
+
+--Create a table with date and time columns
+CREATE TABLE Timestamps
+(
+    date_of_access DATE,
+    time_of_access TIME,
+    Day_time DATETIME,
+    timestamp_column TIMESTAMP
+);
+
+--Insert values into the table
+INSERT INTO Timestamps
+VALUES
+(
+    '2018-01-01',
+    '12:00:00',
+    '2018-01-01 12:00:00',
+    '2018-01-01 12:00:00'
+);
+
+--Date-time functions
